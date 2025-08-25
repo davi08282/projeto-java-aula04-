be2785ac72220795079573e8e2cb21d21150d845
Davi <davi.almeida@duque.g12.brgit config --global user.email davi.almeida@duque.g12.br>
2025-08-25 19:40:30 -0300

    Primeiro commit da aplicação

---
+++ Aula04/src/main/java/com/davi/aula04/Aula04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.davi.aula04;
+
+/**
+ *
+ * @author 08282
+ */
+public class Aula04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
